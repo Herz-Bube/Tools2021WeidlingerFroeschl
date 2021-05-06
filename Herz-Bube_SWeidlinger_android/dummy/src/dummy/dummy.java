b888ab5d7e20347aac9202d8fea49798519f60f7
Sebastian Weidlinger <s.weidlinger99@gmx.at>
2021-05-06 19:09:01 +0200

    Resolving conflics (removing a method)

--- Herz-Bube_SWeidlinger_android/dummy/src/dummy/dummy.java
+++ Herz-Bube_SWeidlinger_android/dummy/src/dummy/dummy.java
@@ -7,14 +7,9 @@ public class dummy {
 		String name = "Sebastian";
 	}
 	
-<<<<<<< HEAD
+
 	public void thisIsANewMethod() { 
 		System.out.println("Hopefully producing a conflict");
-=======
-	public void thisIsANewMethod() {
-		int b = 10;
-		System.out.println("I love git");
->>>>>>> 766945e1987d7a5f615cfe3477591af174069477
 	}
 
 }
